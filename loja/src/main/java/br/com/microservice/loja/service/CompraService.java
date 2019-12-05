2473ba376bdf7ed8e7c950c34247dab6f4221fad
huelton <hueltondihon@gmail.com>
2019-12-04 23:14:44 -0300

    implementando feign-client em fornecedor

--- loja/src/main/java/br/com/microservice/loja/service/CompraService.java
+++ loja/src/main/java/br/com/microservice/loja/service/CompraService.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import br.com.microservice.loja.client.FornecedorClient;
 import br.com.microservice.loja.controller.dto.CompraDTO;
 import br.com.microservice.loja.controller.dto.InfoFornecedorDTO;
 
@@ -13,16 +14,13 @@ import br.com.microservice.loja.controller.dto.InfoFornecedorDTO;
 public class CompraService {
 	
 	@Autowired
-	private RestTemplate client;
+	private FornecedorClient fornecedorClient;
 
 	public void realizaCompra(CompraDTO compraDTO) {
 		
-		ResponseEntity<InfoFornecedorDTO> exchange = client.exchange("http://fornecedor/info/"+compraDTO.getEndereco().getEstado(),
-				                                                      HttpMethod.GET, 
-				                                                      null, 
-				                                                      InfoFornecedorDTO.class);
+		InfoFornecedorDTO info = fornecedorClient.getInfoPorEstado(compraDTO.getEndereco().getEstado());
 		
-		System.out.println(exchange.getBody().getEndereco());
+		System.out.println(info.getEndereco());
 		
 	}
 
